b744d9513b6fcda589beaf5f562d60cc8ffe7fbd
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-12-19 19:45:34 -0800

    Update TeleopInterface.java

--- src/main/java/frc/robot/programs/interfaces/TeleopInterface.java
+++ src/main/java/frc/robot/programs/interfaces/TeleopInterface.java
@@ -1,8 +1,14 @@
 package frc.robot.programs.interfaces;
 
 public interface TeleopInterface {
-       /** call this function once to run   `teleop init code */
+       /** 
+       * call this function once to run teleop init code
+       * ONLY CALL ONCE 
+       */
        public void teleopInit();
-       /** call this function repeatedly to run teleop loop code */
+       /** 
+       * call this function repeatedly to run teleop periodic code
+       * CALL REPEATEDLY 
+       */
        public void teleopPeriodic(); 
 }
