4fe2d3f5ea9e89b4776c09d2d4e60e5946f3967b
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-12-21 17:40:59 -0800

    Updated Javadoc

--- FRC-2022/frc/robot/programs/interfaces/TeleopInterface.java
+++ FRC-2022/frc/robot/programs/interfaces/TeleopInterface.java
@@ -1,16 +1,18 @@
 package frc.robot.programs.interfaces;
+
 /**
  * Use this when writing an teleop program
  */
 public interface TeleopInterface {
-       /** 
-       * call this function once to run teleop init code.
-       * <h4> ONLY CALL ONCE </h4>
-       */
+       /**
+        * <h4>ONLY CALL ONCE</h4>
+        * call this function once to run teleop init code.
+        */
        public void teleopInit();
-       /** 
-       * call this function repeatedly to run teleop periodic code.
-       * <h4> CALL REPEATEDLY </h4>
-       */
-       public void teleopPeriodic(); 
+
+       /**
+        * <h4>CALL REPEATEDLY</h4>
+        * call this function repeatedly to run teleop periodic code.
+        */
+       public void teleopPeriodic();
 }
--- FRC-2022/frc/robot/programs/interfaces/TestInterface.java
+++ FRC-2022/frc/robot/programs/interfaces/TestInterface.java
@@ -1,16 +1,18 @@
 package frc.robot.programs.interfaces;
+
 /**
  * Use this when writing an test program
  */
 public interface TestInterface {
-       /** 
-       * call this function once to run test init code.
-       * <h4> ONLY CALL ONCE </h4>
-       */
+       /**
+        * <h4>ONLY CALL ONCE</h4>
+        * call this function once to run test init code.
+        */
        public void testInit();
-       /** 
-       * call this function repeatedly to run test periodic code.
-       * <h4> CALL REPEATEDLY </h4>
-       */
-       public void testPeriodic(); 
+
+       /**
+        * <h4>CALL REPEATEDLY</h4>
+        * call this function repeatedly to run test periodic code.
+        */
+       public void testPeriodic();
 }
